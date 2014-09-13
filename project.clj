3c557bdf19c995f4314c5ecb5a1a5efb6ed11766
James Polera <james@uncryptic.com>
2014-09-12 23:39:32 -0400

    Update license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clj-meetup "0.1.0-SNAPSHOT"
+(defproject clj-meetup "0.1.0"
   :description "A Clojure library for the meetup.com API"
   :url "https://github.com/polera/clj-meetup"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
 				 [http-kit "2.1.16"]
 				 [cheshire "5.3.1"]
